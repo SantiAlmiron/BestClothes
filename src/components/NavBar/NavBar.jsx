03ddaa91f85824e3cdc2b76a841f43c98a606cdf
Santi Almiron <santialmiron99@hotmail.com>
2022-11-26 23:14:07 -0300

    primer commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+
+import "../NavBar/NavBar.css"
+
+const NavBar = ()=>{
+
+    return (
+        <div className="navbar-container">
+            <div>
+                <img src="./img/logofastsneakers(2).png" alt="" />
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Productos</li>
+                <li>Nosotros</li>
+            </ul>
+            <div>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default NavBar;
